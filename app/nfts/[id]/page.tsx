ab739db0d149102b41034f7038f403c065ffac08
Hh440 <harsh100045@gmail.com>
2024-10-23 19:06:19 +0530

    changes

--- app/nfts/[id]/page.tsx
+++ app/nfts/[id]/page.tsx
@@ -1,17 +1,16 @@
 'use client'
 import { useSearchParams } from 'next/navigation';
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import NftDisplay from '@/components/Nfts';
 
 const Nfts = () => {
     const params = useParams();
     const id = Array.isArray(params.id) ? params.id[0] : params.id; 
     const searchParams = useSearchParams();
-    const name = searchParams.get('name'); 
-
+    const name = searchParams.get('name') || '';
     return (
         <div style={{ width: '75vw' }}>
-            {id && <NftDisplay id={id} name={name}/>}
+            {id && <NftDisplay id={id} name={name} />}
         </div>
     );
 }
