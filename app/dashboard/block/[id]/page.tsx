5022c19917ac2b3db99d43ad93a8fe910d8dc173
Hh440 <harsh100045@gmail.com>
2024-10-29 23:08:50 +0530

    blockDetails

--- app/dashboard/block/[id]/page.tsx
+++ app/dashboard/block/[id]/page.tsx
@@ -1,5 +1,14 @@
+'use client'
 import BlockExplorer from "@/components/block-explorer"
+import { useParams } from "next/navigation"
 
 export default function Block() {
-    return <BlockExplorer/>
+
+    const params= useParams()
+    const id = params.id as string
+    return (
+        <div style={{width:"75vw"}}>
+            <BlockExplorer blockHash={id}/>
+        </div>
+    )
 }
